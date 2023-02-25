c0d2f01138c0cd5d67412b6ded2b2a3eb8f460cb
Sudipta Karmakar <ksudipto329@gmail.com>
2023-02-25 15:10:27 +0530

    first commit

---
+++ Get_Node_Value.py
@@ -0,0 +1,76 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def getNode(llist, posi):
+    # Write your code here
+    n = llist
+    count = 0
+    j = []
+    if n.next is None and count == 0:
+        count += 1
+        j.append(n.data)
+    else:
+        while(n.next is not None):
+            j.append(n.data)
+            n = n.next
+    j.append(n.data)
+    a = len(j)
+    node_value = j[a-posi-1]
+    return str(node_value)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
