b811690016b31e7f11b9588aeb12046556f355ea
Sudipta Karmakar <ksudipto329@gmail.com>
2023-03-13 22:20:57 +0530

    first commit

--- find-merged-point-2-linked-list.py
+++ find-merged-point-2-linked-list.py
@@ -1,3 +1,70 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def findMergeNode(head1, head2):
+    p1,p2 = head1,head2
+    a = b = 0
+    while p1:
+        a += 1
+        p1 = p1.next
+    while p2:
+        b += 1
+        p2 = p2.next
+    
+    if a > b:
+        p1l = head1
+        diff = a - b
+        p2s = head2
+    else:
+        p1l = head2
+        diff = b - a
+        p2s = head1
+        
+    i = 0
+    while i < diff:
+        p1l = p1l.next
+        i += 1
+    while p1l != p2s:
+        p1l = p1l.next
+        p2s = p2s.next
+    return p1l.data
+                    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
